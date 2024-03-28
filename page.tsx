41e97983ecec3002aec915a1fc3b8f33812bc332
Bisma <150579746+bismanaeem28@users.noreply.github.com>
2024-03-28 04:51:26 -0700

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     Bisma
-    </main>
-  );
-}
+function Profile() {
+    return <div>Profile</div>;
+  }
+  
+  export default Profile;
